5ccf06f6806cc0ae500986c846cdad2636d641ff
Luis Aguilar <luisaguilar_920@hotmail.com>
2025-01-27 16:07:23 -0600

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  standalone: true,
+  imports: [FontAwesomeModule,ClipboardModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'marco_links';
+
+  constructor(
+    private clipboard: Clipboard
+  ){}
+
+  navigateToUrl(url:string){
+    window.location.href = url;
+  }
+
+  copy(text:string) {
+    this.clipboard.copy(text);
+
+  }
+
 }
